706f0ba4865dc012d9d4fcea5f9ff08dfb05896c
amir <amirazizihosein@gmail.com>
2023-04-16 17:07:16 +0430

    update style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,15 @@ const Footer = () => {
   return (
     <footer className='grid items-end justify-center '>
       <div className='flex pb-8 space-x-8 md:pb-16'>
-        <GrFacebook className='fill-clGrayishBlue scale-150 transition-colors hover:fill-clSoftRed'></GrFacebook>
-        <BsPinterest className='fill-clGrayishBlue scale-150 transition-colors hover:fill-clSoftRed'></BsPinterest>
-        <BsInstagram className='fill-clGrayishBlue scale-150 transition-colors hover:fill-clSoftRed'></BsInstagram>
+        <a href='#'>
+          <GrFacebook className='fill-clGrayishBlue scale-150 transition-colors hover:fill-clSoftRed '></GrFacebook>
+        </a>
+        <a href='#'>
+          <BsPinterest className='fill-clGrayishBlue scale-150 transition-colors hover:fill-clSoftRed'></BsPinterest>
+        </a>
+        <a href='#'>
+          <BsInstagram className='fill-clGrayishBlue scale-150 transition-colors hover:fill-clSoftRed'></BsInstagram>
+        </a>
       </div>
     </footer>
   )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 const specialTime = parseInt(new Date(2023, 3, 30).getTime().toString())
 const Main = () => {
   const [specialDate, setSpecialDate] = useState(specialTime)
@@ -6,6 +6,8 @@ const Main = () => {
   const [hours, setHours] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
+  const secondFront = useRef<HTMLInputElement>(null)
+  const secondBack = useRef<HTMLInputElement>(null)
   useEffect(() => {
     const repeater = setInterval(() => {
       const currentTime = parseInt(new Date().getTime().toString())
@@ -34,84 +36,88 @@ const Main = () => {
           <div className='space-y-5'>
             <div className='card-container overflow-x-hidden'>
               <div className='card-front'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {days > 9 ? days : `0${days}`}
                 </h1>
               </div>
               <div className='card-back'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {days > 9 ? days : `0${days}`}
                 </h1>
               </div>
               <div className='absolute w-3 h-3 rounded-full top-1/2 left-0 bg-clVerydarkBlue_2 -translate-y-1/2 -translate-x-1/2'></div>
               <div className='absolute w-3 h-3 rounded-full top-1/2 right-0 bg-clVerydarkBlue_2 -translate-y-1/2 translate-x-1/2'></div>
+              <div className='absolute w-full h-px bg-clVerydarkBlue_2 top-1/2 left-0 opacity-60'></div>
             </div>
             <p className='card-text uppercase text-clGrayishBlue'>Days</p>
           </div>
           <div className='space-y-5'>
             <div className='card-container overflow-x-hidden'>
               <div className='card-front'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {hours > 9 ? hours : `0${hours}`}
                 </h1>
               </div>
               <div className='card-back'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {hours > 9 ? hours : `0${hours}`}
                 </h1>
               </div>
               <div className='absolute w-3 h-3 rounded-full top-1/2 left-0 bg-clVerydarkBlue_2 -translate-y-1/2 -translate-x-1/2'></div>
               <div className='absolute w-3 h-3 rounded-full top-1/2 right-0 bg-clVerydarkBlue_2 -translate-y-1/2 translate-x-1/2'></div>
+              <div className='absolute w-full h-px bg-clVerydarkBlue_2 top-1/2 left-0 opacity-60'></div>
             </div>
             <p className='card-text uppercase text-clGrayishBlue'>Hours</p>
           </div>
           <div className='space-y-5'>
             <div className='card-container overflow-x-hidden'>
               <div className='card-front'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {minutes > 9 ? minutes : `0${minutes}`}
                 </h1>
               </div>
               <div className='card-back'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {minutes > 9 ? minutes : `0${minutes}`}
                 </h1>
               </div>
               <div className='absolute w-3 h-3 rounded-full top-1/2 left-0 bg-clVerydarkBlue_2 -translate-y-1/2 -translate-x-1/2'></div>
               <div className='absolute w-3 h-3 rounded-full top-1/2 right-0 bg-clVerydarkBlue_2 -translate-y-1/2 translate-x-1/2'></div>
+              <div className='absolute w-full h-px bg-clVerydarkBlue_2 top-1/2 left-0 opacity-60'></div>
             </div>
             <p className='card-text uppercase text-clGrayishBlue'>Minutes</p>
           </div>
-          <div className='space-y-5'>
-            <div className='card-container overflow-x-hidden'>
-              <div className='card-front'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+          <div className='space-y-5 relative'>
+            <div className='card-container '>
+              <div ref={secondFront} className='card-front  card-display'>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {seconds > 9 ? seconds : `0${seconds}`}
                 </h1>
               </div>
-              <div className='card-back'>
-                <div className='card bg-gray-800 rounded-md rounded-b-lg '></div>
-                <div className='card bg-gray-700 rounded-md rounded-t-lg'></div>
+              <div ref={secondBack} className='card-back '>
+                <div className='card bg-gray-800 rounded-md'></div>
+                <div className='card card-bottom bg-gray-700 rounded-md border-b-8 rounded-b-xl border-b-clVerydarkBlue_2 '></div>
                 <h1 className='absolute top-1/2 left-1/2 text-clSoftRed -translate-x-1/2 -translate-y-1/2 text-4xl opacity-90 md:text-7xl'>
                   {seconds > 9 ? seconds : `0${seconds}`}
                 </h1>
               </div>
-              <div className='absolute w-3 h-3 rounded-full top-1/2 left-0 bg-clVerydarkBlue_2 -translate-y-1/2 -translate-x-1/2'></div>
-              <div className='absolute w-3 h-3 rounded-full top-1/2 right-0 bg-clVerydarkBlue_2 -translate-y-1/2 translate-x-1/2'></div>
+              <div className='absolute w-3 h-3 rounded-full top-1/2 left-0 bg-clVerydarkBlue_2 -translate-y-1/2 -translate-x-1/2 z-10'></div>
+              <div className='absolute w-3 h-3 rounded-full top-1/2 right-0 bg-clVerydarkBlue_2 -translate-y-1/2 translate-x-1/2 z-10'></div>
+              <div className='absolute w-full h-px bg-clVerydarkBlue_2 top-1/2 left-0 opacity-60'></div>
             </div>
             <p className='card-text uppercase text-clGrayishBlue'>Seconds</p>
           </div>
